e52d474dfba370ebeb9010bd49b2026b0f47fd26
Mats Dale <14minerboe16@gmail.com>
2017-09-11 11:18:29 -0600

    Implement average age of green eyed students

--- students.rb
+++ students.rb
@@ -4,8 +4,24 @@ ages = []
 
 File.open("student_data.csv").each do |line|
 	info = line.split(",")
-	students.push(info[0]).strip)
+	students.push(info[0].strip)
 	eye_colors.push(info[1].strip)
 	ages.push(info[2].strip.to_i)
 end
+
+def avg_age_green(students, eye_colors, ages)
+	green_eyes = 0
+	total = 0
+	eye_colors.each_with_index do |color, i|
+		if color == "Green"
+			total += ages[i]
+			green_eyes += 1.0
+
+		end
+	end
+	return total/green_eyes
+
+end
+
+puts avg_age_green(students, eye_colors, ages)
 
