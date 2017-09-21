7a3945361546737a25dd94d4c57f7f515d85ad14
Mats Dale <14minerboe16@gmail.com>
2017-09-20 20:11:12 -0600

    Add students_drive and green_girls

--- students.rb
+++ students.rb
@@ -11,6 +11,83 @@ File.open("student_data.csv").each do |line|
 	blood_types.push(info[3].strip)
 end
 
+def students_drive(ages, students)
+    students_who_can_drive = []
+    index = 0
+    ages.each_with_index do |age, i|
+        if age >= 16
+            students_who_can_drive.push(students[i])
+        end
+    end
+		return students_who_can_drive
+end
+
+students_drive_list = students_drive(ages, students)
+print "The students who can drive are #{students_drive_list}.", "\n"
+
+def green_girls(eye_colors, students)
+    students_with_green = []
+    green_index = 0
+
+    eye_colors.each do |eye_color|
+        if eye_color == "Green"
+            students_with_green.push(green_index)
+        end
+        green_index += 1
+    end
+
+    student_index = 0
+    girl_green_index = 0
+    girl_students_with_green = []
+
+    students.each do |student|
+        if student_index == students_with_green[girl_green_index]
+            girl_green_index += 1
+            if student != "Bob" && student != "Dan" && student != "Frank" && student != "Hank" && student != "Jake" && student != "Lamar" && student != "Nathan" && student != "Patrick" &&student != "Qamra" && student != "Richard" && student != "Siobhan" && student != "Thomas" && student != "Van" && student != "Xavier" && student != "Zed"
+                girl_students_with_green.push(student)
+            end
+        end
+        student_index += 1
+    end
+		return girl_students_with_green
+end
+green_eye_girls = green_girls(eye_colors, students)
+print "The girls with green eyes are #{green_eye_girls}.", "\n"
+
+def sophomore_vowels(students, ages)
+    sophomores = []
+    ages.each_with_index do |age, i|
+        if age == 15
+            sophomores.push(students[i])
+        end
+    end
+
+    vowel_count = []
+    vowels = 0
+    sophomores.each do |sophomore|
+        sophomore.chars.each do |letter|
+            if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
+                vowels += 1
+            end
+        end
+        vowel_count.push(vowels)
+        vowels = 0
+    end
+    max_vowels = vowel_count[0]
+    index = 0
+    vowel_count.each_with_index do |count, i|
+        if count > max_vowels
+            max_vowels = count
+            index = i
+        end
+    end
+    return sophomores[index]
+
+end
+
+sophomore_with_most_vowels = sophomore_vowels(students, ages)
+print "The sophmore name with the most vowels is #{sophomore_with_most_vowels}.", "\n"
+
 def brown_eyes(eye_colors)
 	students_with_brown = 0
 	eye_colors.each do |eye_color|
@@ -53,7 +130,7 @@ def close_to_average(students, eye_colors, ages, avg_age)
 		end
 	end
 	return green_student
-end 
+end
 
 closest_avg_average = close_to_average(students, eye_colors, ages, green_avg)
 print "The green-eyed student that is closest to the average is #{closest_avg_average}", "\n"
@@ -94,7 +171,7 @@ def donor_list(students, blood_types, accepted_bloods)
 	return donors
 end
 
-student_need_blood = "Xavier" 
+student_need_blood = "Xavier"
 donor_blood_type = blood_type(students, blood_types,  student_need_blood)
 accepted_bloods = blood_type_accepted(students, donor_blood_type, student_need_blood)
 student_donor_list = donor_list(students, blood_types, accepted_bloods)
@@ -104,7 +181,7 @@ print "The donor list for #{student_need_blood} is #{student_donor_list}", "\n"
 
 def max_donors(students, blood_types)
 	donor_lengths = []
-	
+
 	students.each_with_index do |student, i|
 		student_blood_type = blood_type(students, blood_types, student)
 		bloods_list = blood_type_accepted(students, student_blood_type, student)
@@ -112,7 +189,7 @@ def max_donors(students, blood_types)
 		donor_lengths.push(student_donor_list.length)
 
 	end
-	
+
 	index = 0
 	donor_max = donor_lengths[0]
 	max_donor_list = []
@@ -131,4 +208,4 @@ def max_donors(students, blood_types)
 end
 
 max_donor_list = max_donors(students, blood_types)
-print "The student(s) with the most donors is(are) #{max_donor_list[0]} with #{max_donor_list[1]} donors"
+print "The student(s) with the most donors is(are) #{max_donor_list[0]} with #{max_donor_list[1]} donors", "\n"
